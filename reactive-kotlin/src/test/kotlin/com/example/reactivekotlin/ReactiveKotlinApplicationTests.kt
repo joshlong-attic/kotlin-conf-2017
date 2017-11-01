bb369d46c3cf137bb89fb634863484118a2715bf
Josh Long <josh@joshlong.com>
2017-10-31 18:26:15 -0700

    reactive kotlin

---
+++ reactive-kotlin/src/test/kotlin/com/example/reactivekotlin/ReactiveKotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.reactivekotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ReactiveKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
